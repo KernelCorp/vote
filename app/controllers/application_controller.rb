123aa0893ced657228c678cbbe972e65cfb28334
max-konin <mk@kernel-corp.com>
2013-10-16 15:40:16 +0700

    added local change

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :set_locale
 
   rescue_from CanCan::AccessDenied do |e|
     redirect_to root_path, :notice => e.message
+  end
+
+  def default_url_options(options={})
+    logger.debug "default_url_options is passed options: #{options.inspect}\n"
+    if I18n.locale == I18n.default_locale
+      {}
+    else
+      { locale: I18n.locale }
+    end
+  end
+
+  def set_locale
+    I18n.locale = (%w(ru en).include?(params[:locale]) ? params[:locale] : nil) || I18n.default_locale
   end
 end
