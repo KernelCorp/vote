c03f4c6c8c108ca3221b73c5da354bbf105dd0d0
max-konin <mk@kernel-corp.com>
2013-10-16 14:56:52 +0700

    Participant can login through phone number. User's phone has become a unique.

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -6,4 +6,14 @@ class Participant < User
   def role? (role)
     role == :participant
   end
+
+  def find_first_by_auth_conditions (warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(login) = :value OR lower(email) = :value OR lower(phone) = :value",
+                               { :value => login.downcase }]).first
+    else
+      where(conditions).first
+    end
+  end
 end
