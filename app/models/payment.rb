31ea8f83e2082a830349d57958e459ca193873be
max-konin <mk@kernel-corp.com>
2013-10-24 13:32:01 +0700

    approve for payments

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,4 +1,15 @@
 class Payment < ActiveRecord::Base
   belongs_to :user
   attr_accessible :amount, :user
+
+  scope :approved, ->{ where is_approved: true}
+
+  def approve!
+    write_attribute :is_approved , 1
+    save!
+  end
+
+  def approved?
+    read_attribute(:is_approved)
+  end
 end
