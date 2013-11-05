aa2e7b38cac446cf80b0e0d790f9cf57f76638f3
Anton Shubin <signal2564@gmail.com>
2013-11-05 13:34:14 +0700

    Close settings for votings

--- app/controllers/votings_controller.rb
+++ app/controllers/votings_controller.rb
@@ -1,7 +1,7 @@
 class VotingsController < ApplicationController
   before_filter :authenticate_participant!, :only => [ :show, :info_about_number, :join ]
   before_filter :authenticate_organization!, :only => [ :new, :create, :edit, :update ]
-  #load_and_authorize_resource
+  before_filter :close_settings  #load_and_authorize_resource
 
   def new
     @voting = Voting.new
@@ -56,7 +56,8 @@ class VotingsController < ApplicationController
   end
 
   private
-  def who
-    @who = current_user
+
+  def close_settings
+    @close_settings = true
   end
 end
