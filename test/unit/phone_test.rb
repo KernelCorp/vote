9c4c5ce1a52933545ed71ac923a488776acd37f1
signal2564 <signal2564@gmail.com>
2013-11-07 22:20:33 +0700

    Voting return sorted phones for paticipant, also returning array of ratings for phones

--- test/unit/phone_test.rb
+++ test/unit/phone_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
 class PhoneTest < ActiveSupport::TestCase
-  test "[]" do
-    phone = phones :one
+  test '[]' do
+    phone = phones :middlebrow_first
     (1..10).each do |i|
       assert phone[i] == phone.number[i]
     end
