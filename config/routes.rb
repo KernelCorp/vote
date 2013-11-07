5149f3392b40d47523e1255ff366c3a5c2466b79
Maganov <vonagam@gmail.com>
2013-11-07 14:58:26 +0700

    new_votings style fix

--- config/routes.rb
+++ config/routes.rb
@@ -30,7 +30,7 @@ Vote::Application.routes.draw do
   get '/org', to: 'main#org'
   ActiveAdmin.routes(self)
 
-  resources :votings, :monetary_votings, :other_votings, path: :votings, as: :voting, controller: :votings do
+  resources :votings, :monetary_votings, :other_votings, path: :votings, controller: :votings do
     post 'info/:number/at/:position' => 'votings#info_about_number', :as => :number_info_at_position_for
     resources :claims, :only => [:create] do
       collection do
