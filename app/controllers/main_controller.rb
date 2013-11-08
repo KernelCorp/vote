5a1934655d91ab2e53567339e7f917648d8d356e
max-konin <mk@kernel-corp.com>
2013-11-08 18:08:11 +0700

    redirect for org

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,8 +1,10 @@
 class MainController < ApplicationController
   def org
+    redirect_to organization_path unless current_organization.nil?
   end
 
   def index
     redirect_to votings_participant_path unless current_participant.nil?
+    redirect_to organization_path        unless current_organization.nil?
   end
 end
