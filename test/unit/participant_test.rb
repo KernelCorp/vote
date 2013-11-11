6af86b59d09037d00d8c879bbe176ea11056c266
max-konin <mk@kernel-corp.com>
2013-11-11 16:13:02 +0700

    refactor debit method. add type to votings

--- test/unit/participant_test.rb
+++ test/unit/participant_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class ParticipantTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+ test 'debit!' do
+   participant = users :middlebrow
+   old_balacne = participant.billinfo
+   participant.debit!(old_balacne / 2)
+   assert participant.billinfo == (old_balacne / 2)
+
+   old_balacne = participant.billinfo
+   assert_raise (Exceptions::PaymentRequiredError) {participant.debit!(old_balacne * 2)}
+   assert participant.billinfo == old_balacne
+ end
 end
