49ce69dcf8fdacdb8eb7c838c4dfca9f96a0e0c6
Anton Shubin <signal2564@gmail.com>
2013-12-05 19:03:51 +0700

    Fix: admin can see who and when use promo #vt-8

--- app/admin/promo.rb
+++ app/admin/promo.rb
@@ -1,3 +1,22 @@
 ActiveAdmin.register Promo do
 
+  show do |p|
+    attributes_table do
+      row :code
+      row :amount
+      row :date_end
+    end
+
+    panel t('promo.promo_uses') do
+      table_for PromoUses.where(promo_id: p.id) do |t|
+        t.column t('activerecord.attributes.promo_uses.participant') do |pu|
+          link_to pu.participant.fullname, admin_participant_path(pu.participant)
+        end
+        t.column t('activerecord.attributes.promo_uses.created_at') do |pu|
+          l pu.created_at, format: :long
+        end
+      end
+    end
+  end
+
 end
--- app/models/promo.rb
+++ app/models/promo.rb
@@ -1,6 +1,8 @@
 class Promo < ActiveRecord::Base
   attr_accessible :amount, :code, :date_end
 
+  has_many :promo_uses, class_name: PromoUses
+
   validates :code, :date_end, presence: true
   validates :code, uniqueness: true
 
