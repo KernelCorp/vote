491cc2fc9249a6633c06c7980b15624f33592bc0
Maganov <vonagam@gmail.com>
2013-12-13 13:37:10 +0700

    admin: votings: added color pickers

--- app/admin/monetary_votings.rb
+++ app/admin/monetary_votings.rb
@@ -4,17 +4,19 @@ ActiveAdmin.register MonetaryVoting do
   form do |f|
     f.inputs do
       f.input :name
-      f.input :prize , :as => :file
-      f.input :brand , :as => :file
-      f.input :prize1, :as => :file
-      f.input :prize2, :as => :file
-      f.input :prize3, :as => :file
-      f.input :custom_background , :as => :file
-      f.input :cost
-      f.input :timer
+      f.input :prize , as: :file
+      f.input :brand , as: :file
+      f.input :prize1, as: :file
+      f.input :prize2, as: :file
+      f.input :prize3, as: :file
+      f.input :custom_head_color, as: :color
+      f.input :custom_background_color, as: :color
+      f.input :custom_background, as: :file
       f.input :status,
               as: :select,
               collection: Hash[Voting::STATUSES.map { |k,v| [k, t("status.#{v}")]}].invert
+      f.input :cost
+      f.input :timer
       f.input :start_date
       f.input :end_date
       f.input :max_users_count, min: 1
@@ -43,19 +45,26 @@ ActiveAdmin.register MonetaryVoting do
   show do |voting|
     attributes_table do
       row :name
+      row :brand  do image_tag voting.brand.url :thumb end
       row :prize  do image_tag voting.prize.url  :thumb end
       row :prize1 do image_tag voting.prize1.url :thumb end
       row :prize2 do image_tag voting.prize2.url :thumb end
       row :prize3 do image_tag voting.prize3.url :thumb end
-      row :brand  do
-        image_tag voting.brand.url :thumb
+      row :custom_head_color do 
+        content_tag( :div, nil, style: "width: 220px; height: 50px; background: #{voting.custom_head_color};" )
+      end
+      row :custom_background_color do 
+        content_tag( :div, nil, style: "width: 220px; height: 50px; background: #{voting.custom_background_color};" )
+      end
+      row :custom_background do 
+        image_tag voting.custom_background.url, height: 165 
       end
       row :start_date
       row :end_date
       row :cost
       row :timer
       row :status do |v|
-              t("status.#{v.status}")
+        t("status.#{v.status}")
       end
       row :organization do |voting|
         link_to voting.organization.org_name, admin_organization_path(voting.organization)
