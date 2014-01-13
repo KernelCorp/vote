609c51d4f304f1c40377499a8b667c8900a313f9
max-konin <mk@kernel-corp.com>
2014-01-13 16:31:01 +0700

    auth for action

--- app/controllers/actions_controller.rb
+++ app/controllers/actions_controller.rb
@@ -2,17 +2,22 @@ class ActionsController < ApplicationController
   before_filter :set_access_control_headers
 
   def do
+    org = Organization.find_by_email params[:login]
     action = Action.find(params[:action_id])
-    fail 'you can not do this action' if action.can_do?
-    stranger_hash = { }
-    Stranger.attribute_names.each { |n| stranger_hash[n] = params[n] unless params[n].nil? }
-    stranger = Stranger.find_or_create_by_email stranger_hash['email']
-    stranger.update_attributes! stranger_hash
-    done_thing = stranger.done_things.create do |dt|
-      dt.voting_id = action.voting_id
-      dt.what_id = action.id
+    if !org.nil? && org.valid_password?(params[:password]) && org.votings.include?(action.other_voting)
+      fail 'you can not do this action' if action.can_do?
+      stranger_hash = { }
+      Stranger.attribute_names.each { |n| stranger_hash[n] = params[n] unless params[n].nil? }
+      stranger = Stranger.find_or_create_by_email stranger_hash['email']
+      stranger.update_attributes! stranger_hash
+      done_thing = stranger.done_things.create do |dt|
+        dt.voting_id = action.voting_id
+        dt.what_id = action.id
+      end
+      render json: { status: 'ok', thing: 'done' }
+    else
+      head :unauthorized
     end
-    render json: { status: 'ok', thing: 'done' }
   rescue => e
     render json: { success: false, status: 'failure', errors: e.message}
   end
--- test/functional/actions_controller_test.rb
+++ test/functional/actions_controller_test.rb
@@ -4,22 +4,40 @@ class ActionsControllerTest < ActionController::TestCase
 
   test 'do thing by stranger' do
     action = actions(:first_action)
-    post :do, action_id: action.id, email: 'test@test.test', phone: '1334567893'
-
-    stranger = Stranger.where(email: 'test@test.test').first
-    done_thing = WhatDone.where(who_id: stranger.id)
-    assert !stranger.nil? && !done_thing.nil?
+    org    = action.other_voting.organization
+    post :do, action_id: action.id, email: 'test@test.test', phone: '1334567893', login: org.email,
+         password: 'jobspass'
     assert_response :success
+    stranger = Stranger.where(email: 'test@test.test').first
+    assert !stranger.nil?
+    done_thing = WhatDone.where(who_id: stranger.id)
+    assert !done_thing.nil?
   end
 
   test 'addition stranger data' do
     action = actions(:first_action)
+    org    = action.other_voting.organization
     post :do, action_id: action.id, email: 'test@test.test'
-    post :do, action_id: action.id, email: 'test@test.test', phone: '1334567893'
+    post :do, action_id: action.id, email: 'test@test.test', phone: '1334567893', login: org.email,
+         password: 'jobspass'
     assert_response :success
     strangers = Stranger.where(email: 'test@test.test')
     assert strangers.count == 1
     assert_equal strangers.first.phone, '1334567893'
+  end
+
+  test 'access denied if orgs data is invalid' do
+      action = actions(:first_action)
+      org    = action.other_voting.organization
+      post :do, action_id: action.id, email: 'test@test.test', phone: '1334567893', login: org.email,
+           password: 'invalid pass'
+      assert_response 401
+  end
+
+  test 'access denied without org'  do
+    action = actions(:first_action)
+    post :do, action_id: action.id, email: 'test@test.test'
+    assert_response 401
   end
 
 end
