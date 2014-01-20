9eeabf4b4876312425e6699d2c481a11c1d578db
max-konin <mk@kernel-corp.com>
2014-01-20 18:07:15 +0700

    revert create for vk posts

--- app/controllers/vk_posts_controller.rb
+++ app/controllers/vk_posts_controller.rb
@@ -2,11 +2,8 @@ class VkPostsController < ApplicationController
   before_filter :authenticate_participant!
 
   def create
-    post_id = params[:vk_post][:link].match(/-?\d+_\d+/)[0]
-
-    @post = current_user.vk_posts.build post_id: post_id
+    @post = current_user.vk_posts.build params[:vk_post]
     @post.voting = OtherVoting.find params[:other_voting_id]
-    @post.participant = current_participant
     if @post.save
       redirect_to @post.voting
     else
