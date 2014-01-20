a671f868224c683d9e2a2ffdadced8787612ee32
Dmitry Maganov <vonagam@gmail.com>
2014-01-20 17:47:15 +0700

    other voting show

--- app/controllers/other_votings_controller.rb
+++ app/controllers/other_votings_controller.rb
@@ -1,7 +1,10 @@
 class OtherVotingsController < ApplicationController
   layout Proc.new { |controller| controller.participant_signed_in? ? 'participants' : 'voting_any_who' }
+  
   def show
     @voting = OtherVoting.find params[:id]
+    @voting_participants = @voting.sorted_participants
+
     if @voting.can_vote_for_claim?
       render 'show_active'
     else
