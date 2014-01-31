1146c22acd97286ca76397bd5397a8b2455cb43a
max-konin <mk@kernel-corp.com>
2014-01-31 12:58:04 +0700

    html safe text

--- app/admin/vk_post.rb
+++ app/admin/vk_post.rb
@@ -5,7 +5,9 @@ ActiveAdmin.register VkPost do
     attributes_table do
       row :id
       row :post_id
-      row :text
+      row :text do |post|
+        raw post.text
+      end
       row :participant do |post|
         link_to post.participant.fullname, admin_participant_path(post.participant)
       end
