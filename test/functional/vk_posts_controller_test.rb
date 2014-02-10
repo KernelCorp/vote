1ee90547f1d6deb48f3cc14a8ddff05ead665cbf
max-konin <mk@kernel-corp.com>
2014-02-10 13:49:34 +0700

    test create by url

--- test/functional/vk_posts_controller_test.rb
+++ test/functional/vk_posts_controller_test.rb
@@ -1,11 +1,17 @@
 require 'test_helper'
 
 class VkPostsControllerTest < ActionController::TestCase
-  test "should create" do
+  test 'should created by id' do
     sign_in :participant, users(:one_time_pass)
     voting = votings :other_voting
     post :create, {other_voting_id: voting, vk_post: {post_id: '3793114_151'}}
     assert_redirected_to other_voting_path(voting)
   end
 
+  test 'should created by valid url' do
+    sign_in :participant, users(:one_time_pass)
+    voting = votings :other_voting
+    post :create, {other_voting_id: voting, vk_post: {url: 'http://vk.com/id3793114?w=wall3793114_151%2Fall'}}
+    assert_redirected_to other_voting_path(voting)
+  end
 end
