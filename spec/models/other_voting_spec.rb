6e6ab4ef4530cf28b56fbee1678fb0cef928a7a1
Dmitry Maganov <vonagam@gmail.com>
2014-02-20 19:59:02 +0700

    change other voting spec

--- spec/models/other_voting_spec.rb
+++ spec/models/other_voting_spec.rb
@@ -1,38 +1,42 @@
 require 'spec_helper'
 
 describe OtherVoting do
+  let(:voting) { votings :other_voting }
+  let(:user)   { users :middlebrow }
+  before do
+    FactoryGirl.create :vk_post, voting: voting, participant: user, url: 'http://vk.com/feed?w=wall-34580489_20875'
+    FactoryGirl.create :tw_post, voting: voting, participant: user, url: 'https://twitter.com/alexmak/status/436459004651642881'
+  end
+
   it 'complete if necessary' do
-    voting = votings(:other_voting)
     voting.update_attributes! end_date: Date.today
     old_users = voting.participants.dup
     voting.complete_if_necessary!
 
     :prizes.should == voting.reload.status
-    voting.social_posts.each { |p| assert !p.result.nil? }
+
+    puts voting.social_posts.count
+
+    voting.social_posts.each { |p| assert !p.points.nil? }
     old_users.each do |user|
       new_user = User.find user
       sum = 0
-      voting.social_posts.where(participant_id: user).each { |p| sum += p.result}
+      voting.social_posts.where(participant_id: user).each { |p| sum += p.points }
       new_user.billinfo.should == (user.billinfo + sum)
     end
 
   end
 
   it 'count point for participant' do
-    voting = votings :other_voting
-    user   = users :middlebrow
-    points = voting.count_point_for user
+    points = voting.participant_points user
 
-    2.should == points
+    expect( points ).to be >= 0
   end
 
   it 'get sorting participants' do
-    voting = votings :other_voting
-    user   = users :middlebrow
-    actual = voting.sorted_participants
-
-    assert_equal actual.map {|u| u.id}, [users(:new).id, user.id]
-    actual.first.points.should == 10
+    voting.sorted_participants.each do |p|
+      expect( p.points ).not_to be_nil
+    end
   end
 
   it 'get population' do
