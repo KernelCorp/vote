5525b5309c162c5c8a7a145570a5932116724ec0
Dmitry Maganov <vonagam@gmail.com>
2014-04-08 18:35:45 +0700

    test on task for default strategy for old votings

--- spec/tasks/vote_rake_spec.rb
+++ spec/tasks/vote_rake_spec.rb
@@ -1,51 +1,60 @@
 require 'spec_helper'
 require 'rake'
 
-describe 'proff namespace rake task' do
+describe 'rake' do
   before :all do
     Rake.application.rake_require 'tasks/vote'
     Rake::Task.define_task(:environment)
   end
 
-  describe 'vote:snapshot' do
-    def run_task(task, args = nil)
-      Rake::Task[task].reenable
-      Rake.application.invoke_task "#{task}#{ args && "[#{args}]" }"
+  def run_task(task, args = nil)
+    Rake::Task[task].reenable
+    Rake.application.invoke_task "#{task}#{ args && "[#{args}]" }"
+  end
+
+  before :each do
+    allow_any_instance_of(OtherVoting).to receive(:social_snapshot).and_return(true)
+  end
+
+  it 'invoke social_snapshot' do
+    OtherVoting.first.update_attributes snapshot_frequency: :daily
+    expect_any_instance_of(OtherVoting).to receive(:social_snapshot).and_return(true)
+    run_task 'vote:snapshot', 'daily'
+  end
+
+  it 'old points to states' do
+    voting = OtherVoting.first
+    actions = Social::Action::Tw.where( voting_id: voting.id ).first
+
+    actions.update_attributes like_points: 3, repost_points: 7
+
+    voting.update_attributes status: :close
+
+    @post = FactoryGirl.create :tw_post, voting: voting, participant: users(:middlebrow), url: 'https://twitter.com/Politru_project/status/451559020654886912'
+    
+    def points_to( points, likes, reposts )
+      @post.update_attributes points: points
+      run_task 'vote:states_for_finished'
+      state = @post.states.last
+      expect( state[:likes] ).to eq(likes)
+      expect( state[:reposts] ).to eq(reposts)
     end
 
-    before :each do
-      allow_any_instance_of(OtherVoting).to receive(:social_snapshot).and_return(true)
-    end
+    points_to 49, 0, 7
+    points_to 16, 3, 1
+    points_to 13, 4, 0
+  end
 
-    it 'invoke social_snapshot' do
-      OtherVoting.first.update_attributes snapshot_frequency: :daily
-      expect_any_instance_of(OtherVoting).to receive(:social_snapshot).and_return(true)
-      run_task 'vote:snapshot', 'daily'
-    end
+  it 'strategy_for_old' do
+    voting = OtherVoting.first
 
-    it 'old points to states' do
-      voting = OtherVoting.first
-      actions = Social::Action::Tw.where( voting_id: voting.id ).first
+    voting.strategy.delete_all if voting.strategy
 
-      actions.update_attributes like_points: 3, repost_points: 7
+    expect( voting.strategy ).to be(nil)
 
-      voting.update_attributes status: :close
+    run_task 'vote:strategy_for_old'
 
-      @post = FactoryGirl.create :tw_post, voting: voting, participant: users(:middlebrow), url: 'https://twitter.com/Politru_project/status/451559020654886912'
-      
-      def points_to( points, likes, reposts )
-        @post.update_attributes points: points
-        run_task 'vote:states_for_finished'
-        state = @post.states.last
-        expect( state[:likes] ).to eq(likes)
-        expect( state[:reposts] ).to eq(reposts)
-      end
-
-      points_to 49,  0, 7
-      points_to 16,  3, 1
-      points_to 13,  4, 0
-    end
-
+    expect( OtherVoting.first.strategy ).not_to be(nil)
   end
   
 end
