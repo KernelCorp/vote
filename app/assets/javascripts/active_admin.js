6bcac53b9ef8b41292f6ad201f50dbd95d2978b5
Dmitry Maganov <vonagam@gmail.com>
2014-04-09 18:49:49 +0700

    post graphic timezone

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -4,6 +4,12 @@
 //= require highcharts
 //= require chartkick
 
+Highcharts.setOptions({
+  global: {
+    timezoneOffset: 7 * 60
+  }
+});
+
 $(function(){
   $('.js_timer').each(function(){
     new Timer( $(this) );
