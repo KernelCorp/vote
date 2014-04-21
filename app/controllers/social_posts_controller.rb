19716bc841da4785a6f1d9976e5f9eda1f059c16
max-konin <mk@kernel-corp.com>
2014-04-21 18:42:54 +0700

    hot fix

--- app/controllers/social_posts_controller.rb
+++ app/controllers/social_posts_controller.rb
@@ -2,14 +2,14 @@ class SocialPostsController < ApplicationController
   before_filter :authenticate_participant!
 
   def create
-    type = params[:social_post].delete :type
+    type = params[:social_post_base].delete :type
 
     if type.empty? || (klass = type.safe_constantize).nil?
       return render json: { _success: false, _resource: 'social_post',
                             _errors: { type: [I18n.t('activerecord.errors.models.social_post.type.net_not_exist')] } }
     end
 
-    post = klass.new params[:social_post]
+    post = klass.new params[:social_post_base]
 
     post.participant_id = current_participant.id
     post.voting_id = OtherVoting.find(params[:other_voting_id]).id
