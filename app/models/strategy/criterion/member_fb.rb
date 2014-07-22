c180591afc6e9456b33854a630c7a5a7855bf9ec
Dmitry Maganov <vonagam@gmail.com>
2014-07-22 17:06:47 +0700

    continue

--- app/models/strategy/criterion/member_fb.rb
+++ app/models/strategy/criterion/member_fb.rb
@@ -9,11 +9,13 @@ class Strategy::Criterion::MemberFb < Strategy::Criterion::Base
 
   def get_group_members
     Rails.cache.fetch "group_members_fb_#{group_id}", expires_in: 12.hour do
-      Social::Post::Fb.FB[:api]
-      .fql_query("SELECT uid FROM group_member WHERE gid = \"#{group_id}\"")
-      .map do |member| 
-        member['uid']
-      end
+      fb = Social::Post::Fb.FB[:api]
+
+      gid = fb.fql_query "SELECT id FROM profile WHERE username = \"#{group_id}\" OR id = \"#{group_id}\""
+
+      members = fb.fql_query "SELECT uid FROM group_member WHERE gid = \"#{gid[0]['id']}\""
+
+      members.map{ |member| member['uid'] }
     end
   end
 end
